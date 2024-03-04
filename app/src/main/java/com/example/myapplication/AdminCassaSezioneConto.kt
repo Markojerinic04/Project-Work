16bbd4de989e9c9199de0029d9b020fdd413f568
FrigoAndrea <8293314@itisrossi.vi.it>
2024-03-04 16:32:55 +0100

    ProgettoAggiornato0303

---
+++ app/src/main/java/com/example/myapplication/AdminCassaSezioneConto.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminCassaSezioneConto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_cassa_sezione_conto)
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val HomePageAdmin = findViewById<Button>(R.id.second_activity_btn)
+        HomePageAdmin.setOnClickListener{
+            val Intent = Intent(this, HomeAdmin::class.java)
+            startActivity(Intent)
+        }
     }
 }
